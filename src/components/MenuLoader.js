2b8a45f5b600d9b6c85d4c49214011bc5be7e822
Etienne Dldc <e.deladonchamps@gmail.com>
2019-05-13 10:52:58 +0200

    Fix

--- src/components/MenuLoader.js
+++ src/components/MenuLoader.js
@@ -3,11 +3,15 @@ import React from 'react';
 const MenuLoader = () => {
   return (
     <div className="MenuLoader">
-      <div className="MenuLoader--menu">
+      <div className="MenuLoader--group">
         <div className="MenuLoader--name" />
-        <div className="MenuLoader--items">
-          <div className="MenuLoader--item" />
-          <div className="MenuLoader--item" />
+      </div>
+      <div className="MenuLoader--items">
+        <div className="MenuItem">
+          <div className="MenuItem--card" />
+        </div>
+        <div className="MenuItem">
+          <div className="MenuItem--card" />
         </div>
       </div>
     </div>
